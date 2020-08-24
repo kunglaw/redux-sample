316af764cb505a5be8041b1dd84320b168a69cc0
Aries Dimas Yudhistira <dimas@baezeni.com>
2020-08-24 19:46:13 +0700

    first init

--- src/index.js
+++ src/index.js
@@ -4,9 +4,51 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from "redux"
+import {Provider} from "react-redux"
+import allReducer from "./reducers"
+
+// // Store 
+
+// // action 
+// const increment = () => {
+//   return {
+//     type:"INCREMENT"
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type:"DECREMENT"
+//   }
+// }
+
+// // reducer 
+// const counter = (state = 0, action) => {
+//     switch(action.type) {
+//       case "INCREMENT":
+//         return state + 1
+//       case "DECREMENT":
+//         return state - 1
+//     }
+// }
+
+let store = createStore(allReducer)
+
+// store.subscribe(() => console.log(store.getState()))
+
+//DISPATCH
+// store.dispatch(increment())
+// store.dispatch(decrement())
+// store.dispatch(decrement())
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
